0ec6ff36d387496e7c477b5ce70f9e58bb3a2c39
neil patel <neil@myindianweddingapp.com>
2019-02-28 19:42:38 +0000

    Added a net

--- sketch.js
+++ sketch.js
@@ -3,5 +3,14 @@ function setup() {
 }
 
 function draw() {
+  // colour of the line
   background (0)
+  // size and thickness of the line
+  stroke(255)
+  strokeWeight(1)
+
+  // add lines for the net
+  for (var i = 0; i < height/10; i++) {
+    line(width/2, height/10*i, width/2, height/20 + height/10*i)
+  }
 }
